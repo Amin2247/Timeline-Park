b2aa13e607ab6984a3e8be746d0e6e9b6811cf96
Amin2247 <101484972+Amin2247@users.noreply.github.com>
2022-04-16 16:02:05 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+background(220);
+line(400, 200, 0, 200);
+stroke(126);
+textSize(6);
+text('I plan to code funtionality this monday at April 18th', 20, 190);
+textSize(6);
+text('I plan to Making and Implementing Assets/Art at April 21th', 40,210);
+text('plan to debug at Debugging and Testing Software at April 23', 160, 190);
+text('extra features after April 23rd ', 270, 210);
+textSize(6);
+  
+}
